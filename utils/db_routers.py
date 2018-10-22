559760f97459ee4faf061ff3322ec3b26c0ed020
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-10-22 19:20:25 +0200

    add ModelDBRouter

--- utils/db_routers.py
+++ utils/db_routers.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from django.conf import settings
 
 
@@ -22,10 +23,27 @@ class AppDBRouter:
         return settings.APP_DB_ROUTER.get(app_label)
 
     def db_for_write(self, model, **hints):
-        app_label = model._meta.app_label
-        return settings.APP_DB_ROUTER.get(app_label)
+        return self.db_for_read(model, **hints)
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         _db = settings.APP_DB_ROUTER.get(app_label)
         if _db is not None:
             return db == _db
+
+
+class ModelDBRouter:
+    def db_for_read(self, model, **hints):
+        model_label = model._meta.label
+        return settings.MODEL_DB_ROUTER.get(model_label)
+
+    def db_for_write(self, model, **hints):
+        return self.db_for_read(model, **hints)
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if model_name:
+            model = apps.get_model(app_label, model_name)
+            _db = self.db_for_write(model)
+            if _db:
+                return db == _db
+            elif db in settings.MODEL_DB_ROUTER.values():
+                return False
