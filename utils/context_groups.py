3eef57488bf5de9ac1009ec5e92b3f0883fcddae
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-10-26 14:13:42 +0200

    add context groups

---
+++ utils/context_groups.py
@@ -0,0 +1,94 @@
+from django import template
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+from django.contrib.auth import mixins as auth_mixins
+
+register = template.Library()
+
+
+class Context(models.Model):
+    pass
+
+
+class ContextGroup(models.Model):
+    name = models.CharField(max_length=80)
+    context = models.ForeignKey(Context, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, blank=True)
+    permissions = models.ManyToManyField(Permission, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_permissions(self):
+        perms = self.permissions.values_list(
+            'content_type__app_label',
+            'codename'
+        )
+        return ['{}.{}'.format(ct, name) for ct, name in perms]
+
+    def has_perm(self, perm):
+        return perm in self.get_permissions()
+
+
+class BasicObjectBackend:
+    # See https://code.djangoproject.com/ticket/20218
+
+    def authenticate(self, username, password):
+        return None
+
+    def get_user_permissions(self, user, obj=None):
+        if obj is None:
+            return set()
+        return user.get_user_permissions()
+
+    def get_group_permissions(self, user, obj=None):
+        if obj is None:
+            return set()
+        return user.get_group_permissions()
+
+    def get_all_permissions(self, user, obj=None):
+        if obj is None:
+            return set()
+        return user.get_all_permissions()
+
+    def has_perm(self, user, perm, obj=None):
+        if obj is None:
+            return False
+        return user.has_perm(perm)
+
+
+class ContextPermissionBackend:
+    def authenticate(self, username, password):
+        return None
+
+    def get_group_permissions(self, user, obj=None):
+        perms = set()
+        if user.is_active and isinstance(obj, Context):
+            groups = ContextGroup.objects.filter(context=obj, users=user)
+            for group in groups:
+                perms.update(group.get_permissions())
+        return perms
+
+    def get_all_permissions(self, user, obj=None):
+        return self.get_group_permissions(user, obj)
+
+    def has_perm(self, user, perm, obj=None):
+        if user.is_active and isinstance(obj, Context):
+            groups = ContextGroup.objects.filter(context=obj, users=user)
+            return any(group.has_perm(perm) for group in groups)
+
+
+class PermissionRequiredMixin(auth_mixins.PermissionRequiredMixin):
+    def get_permission_object(self):
+        return None
+
+    def has_permission(self):
+        perms = self.get_permission_required()
+        permission_object = self.get_permission_object()
+        return self.request.user.has_perms(perms, obj=permission_object)
+
+
+@register.simple_tag
+def has_perm(perm, user, obj=None):
+    return user.has_perm(perm, obj)
