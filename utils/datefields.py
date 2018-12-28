657451e1bdb08e26868985dba4b39bcbd14ecd42
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-12-28 19:16:00 +0100

    fix format for date inputs
    native datepickers do not work with localized formats

--- utils/datefields.py
+++ utils/datefields.py
@@ -2,10 +2,14 @@ from django import forms
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+DATE_FORMAT = '%Y-%m-%d'
+
 DATE_DEFAULTS = {
     'placeholder': _('yyyy-mm-dd'),
     'type': 'date',
 }
+
+TIME_FORMAT = '%H:%M'
 
 TIME_DEFAULTS = {
     'placeholder': _('HH:MM'),
@@ -14,28 +18,33 @@ TIME_DEFAULTS = {
 
 
 class DateInput(forms.DateInput):
-    def __init__(self, attrs={}):
+    def __init__(self, attrs={}, **kwargs):
         defaults = TIME_DEFAULTS.copy()
         defaults.update(attrs)
-        super().__init__(attrs=defaults)
+        super().__init__(format=DATE_FORMAT, attrs=defaults, **kwargs)
 
 
 class TimeInput(forms.TimeInput):
-    def __init__(self, attrs={}):
+    def __init__(self, attrs={}, **kwargs):
         defaults = DATE_DEFAULTS.copy()
         defaults.update(attrs)
-        super().__init__(attrs=defaults)
+        super().__init__(format=TIME_FORMAT, attrs=defaults, **kwargs)
 
 
 class DateTimeInput(forms.SplitDateTimeWidget):
-    def __init__(self, date_attrs={}, time_attrs={}):
+    def __init__(self, date_attrs={}, time_attrs={}, **kwargs):
         date_defaults = DATE_DEFAULTS.copy()
         date_defaults.update(date_attrs)
 
         time_defaults = TIME_DEFAULTS.copy()
         time_defaults.update(time_attrs)
 
-        super().__init__(date_attrs=date_defaults, time_attrs=time_defaults)
+        super().__init__(
+            date_format=DATE_FORMAT,
+            date_attrs=date_defaults,
+            time_format=TIME_FORMAT,
+            time_attrs=time_defaults,
+            **kwargs)
 
 
 class DateFormField(forms.DateField):
